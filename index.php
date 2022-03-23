8c78d6f6c191cd6109b457017fc6cf08fdbb4b48
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-03-23 22:14:00 +0200

    upd: refactored repeating code

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-declare(strict_types = 1);
 
-class Library {
+declare(strict_types=1);
+
+class Library
+{
 
     public array $books;
 
-    public function __construct() {
+    public function __construct()
+    {
         $storageFile = './storage.json';
         if (file_exists($storageFile)) {
             $this->books = json_decode(file_get_contents($storageFile), true);
@@ -15,7 +18,8 @@ class Library {
         }
     }
 
-    public function addBook (string $author, string $title): void {
+    public function addBook(string $author, string $title): void
+    {
         $this->books[] = [
             'author' => $author,
             'title' => $title,
@@ -26,16 +30,34 @@ class Library {
         $this->updateStorage();
     }
 
-    public function removeBook (string $author, string $title): void {
+    public function removeBook(string $author, string $title): void
+    {
+        $remove = function (int $key) {
+            array_splice($this->books, $key, 1);
+        };
+        $this->changeBookStatus($author, $title, $remove);
+    }
+
+    public function checkOutBook(string $author, string $title, string $user): void
+    {
+        $checkOut = function (int $key, array $book, string $user): void {
+            $book['isCheckedOut'] = true;
+            $book['checkedOutTo'] = $user;
+            $this->books[$key] = $book;
+        };
+        $this->changeBookStatus($author, $title, $checkOut, $user);
+    }
+
+    private function changeBookStatus(string $author, string $title, callable $callback, $user = NULL)
+    {
         $bookFound = false;
-       
-        foreach($this->books as $book) {
+        foreach ($this->books as $key => $book) {
             if ($book['author'] === $author && $book['title'] === $title) {
                 $bookFound = true;
                 if ($book['isCheckedOut']) {
                     echo "Book is already checked out!";
                 } else {
-                    array_splice($this->books,array_search($book,$this->books),1);
+                    $callback($key, $book = $book, $user);
                 }
             }
         }
@@ -45,59 +67,39 @@ class Library {
         }
     }
 
-    public function checkOutBook(string $author, string $title, string $user): void {
-
-
-        $bookFound = false;
-        foreach($this->books as $key => $book) {
-            if ($book['author'] === $author && $book['title'] === $title) {
-                $bookFound = true;
-                if ($book['isCheckedOut']) {
-                    echo "Book is already checked out!";
-                } else {
-                    $book['isCheckedOut'] = true;
-                    $book['checkedOutTo'] = $user;
-                    $this->books[$key] = $book;
-                    $this->updateStorage();    
-                }
-            }
-        }
-
-        if (!$bookFound) {
-            echo 'Book not found!';
-        }
-        
-    }
-
-
-    private function updateStorage (): void {
+    private function updateStorage(): void
+    {
         $storageFile = './storage.json';
         file_put_contents($storageFile, json_encode($this->books, JSON_PRETTY_PRINT));
     }
 
-    public function findBooks(string $term, string $method): void {
-        $regex = '/'.trim($term).'/i';
+    public function findBooks(string $term, string $method): void
+    {
+        $regex = '/' . trim($term) . '/i';
         $bookFound = false;
 
-        foreach($this->books as $key => $book) {
-            if (preg_match($regex,$book[$method]) === 1) {
+        foreach ($this->books as $key => $book) {
+            if (preg_match($regex, $book[$method]) === 1) {
                 $bookFound = true;
-                echo PHP_EOL.'---------'.PHP_EOL;
-                echo 'Author: '.$book['author'].PHP_EOL;
-                echo 'Title: '.$book['title'].PHP_EOL;
-                echo 'Checked out?: '.($book['isCheckedOut'] ? 'Yes' : 'No').PHP_EOL;
-                if ($book['isCheckedOut']) {
-                    echo 'Checked out to: '.$book['checkedOutTo'].PHP_EOL;
-                }
+                $this->printResult($book);
             }
         }
 
         if (!$bookFound) {
-            echo 'No items found!';
+            echo 'No books found!';
         }
     }
 
-
+    private function printResult(array $book)
+    {
+        echo PHP_EOL . '---------' . PHP_EOL;
+        echo 'Author: ' . $book['author'] . PHP_EOL;
+        echo 'Title: ' . $book['title'] . PHP_EOL;
+        echo 'Checked out?: ' . ($book['isCheckedOut'] ? 'Yes' : 'No') . PHP_EOL;
+        if ($book['isCheckedOut']) {
+            echo 'Checked out to: ' . $book['checkedOutTo'] . PHP_EOL;
+        }
+    }
 }
 
 
@@ -118,6 +120,7 @@ $inventory = new Library();
 //--- Remove book:
 
 // $inventory->removeBook('writer', 'Eye-cathing');
+// $inventory->removeBook('writer2', 'captivating');
 
 //--- Search books:
 
