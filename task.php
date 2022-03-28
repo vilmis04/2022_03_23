f8ed6bdcde3fda3aa482f931fcc46d55ae54422a
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-03-28 20:42:27 +0300

    upd: lines for testing moved to another file

--- task.php
+++ task.php
@@ -146,29 +146,3 @@ class LibraryReporter {
 
 
 $inventory = new Library();
-
-//Lines for testing:
-
-//--- adding books:
-
-// $inventory->addBook('writer', 'Eye-cathing');
-// $inventory->addBook('writer2', 'captivating');
-
-//--- Check out book:
-
-// $inventory->checkOutBook('writer', 'Eye-cathing', 'is mine now');
-
-//--- Remove book:
-
-// $inventory->removeBook('writer', 'Eye-cathing');
-// $inventory->removeBook('writer2', 'captivating');
-
-//--- Search books:
-
-// $inventory->findBooks('iter', 'author');
-// $inventory->findBooks('ier', 'author');
-// $inventory->findBooks('2', 'author');
-
-// $inventory->findBooks('eye', 'title');
-// $inventory->findBooks('c', 'title');
-// $inventory->findBooks('  vat', 'title');
